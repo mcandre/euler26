1c9f2942c1ea908e6f35eeec5924339e0c7465ab
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-03-23 21:31:28 -0500

    working

---
+++ README.md
@@ -0,0 +1,16 @@
+# euler26 - a long division solution to Project Euler #26
+
+# EXAMPLE
+
+```
+$ time ./euler26.hs
+...
+
+real	0m1.893s
+user	0m1.787s
+sys	0m0.083s
+```
+
+# REQUIREMENTS
+
+* [Haskell](https://www.haskell.org/)
---
+++ euler26.hs
@@ -0,0 +1,18 @@
+#!/usr/bin/env runhaskell
+
+import Data.List (sortOn)
+
+chain (0:_) _ _ = []
+chain dividends dividend divisor
+  | length dividends > 0 && (head dividends) `elem` (tail dividends) = dividends
+  | dividend < divisor = chain dividends (dividend * 10) divisor
+  | otherwise = chain (dv':dividends) dv' divisor
+   where
+     dv' = dividend - divisor * m
+     m = maxMultiple 1 divisor dividend
+
+     maxMultiple multiplicand multiplier limit
+       | (multiplicand + 1) * multiplier > limit = multiplicand
+       | otherwise = maxMultiple (multiplicand + 1) multiplier limit
+
+main = print . head . sortOn ((1 -) . length . (chain [] 1)) $ [1 .. 999]
